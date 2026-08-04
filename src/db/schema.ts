5d4159ea74feacc38ca453025f4b54aeb78afa8e
Igggosha <igggoshastudios@gmail.com>
2026-08-04 16:31:56 +0100

    added exercises, muscles, exercisesTrainMuscles junction tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,38 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import {integer, pgTable, primaryKey, serial, text, timestamp} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: serial("id").primaryKey(),
-    name: text("name").notNull(),
+
     email: text("email").notNull().unique(),
+
+    username: text("username").notNull().unique(),
+
+    passwordHash: text("password_hash").notNull(),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const muscles = pgTable("muscles", {
+    id: serial("id").primaryKey(),
+
+    commonName: text("commonName").notNull().unique(),
+
+    scientificName: text("scientificName"),
+});
+
+export const exercises = pgTable("exercises", {
+    id: serial("id").primaryKey(),
+
+    name: text("name").notNull().unique(),
+});
+
+export const exercisesTrainMuscles = pgTable("exercisesTrainMuscles", {
+    muscleId: integer("muscle_id").notNull().references(() => muscles.id),
+    exerciseId: integer("exercise_id").notNull().references(() => exercises.id),
+}, (table) => ({
+        pk: primaryKey({
+            columns: [table.muscleId, table.exerciseId]
+        }),
+    }),
+);
+
