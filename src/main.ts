dfd574db5450538f6793db9aa4eb56e7cbb8bcde
GboyYouMam <kozahar185@gmail.com>
2026-08-16 16:02:23 +0300

    feat: user things

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   app.use(
     session({
